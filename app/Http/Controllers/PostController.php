069753f938ef32a0e6e0b2dd2fc0d10a725721b3
taka1214 <globar173@gmail.com>
2023-09-06 19:14:54 +0900

    modified css a bit4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,7 +26,7 @@ class PostController extends Controller
         $request->validate([
             'italian' => 'required',
             'japanese' => 'required',
-            'voice_script' => 'required|file|mimes:mp3,wav,ogg,mpeg|max:8000', // 5MB以下のmp3, wav, ogg ファイルを許可
+            'voice_script' => 'required|file|mimes:mp3,wav,ogg,m4a|max:8000', // 5MB以下のmp3, wav, ogg ファイルを許可
             'memo' => 'nullable',
         ]);
 
