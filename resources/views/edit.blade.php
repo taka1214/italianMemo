870be5983f555e9ae0ad2ff40199dfe97d3158f2
taka1214 <globar173@gmail.com>
2023-09-06 22:30:07 +0900

    modified css a bit15

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -22,9 +22,9 @@
         <div>
           <label for="voice_script" class="text-gray-600">音声ファイル</label>
           @if($post->voice_script)
-          <p>現在の音声:
+          <p>
             @if($post->voice_script)
-            <button onclick="playVoiceScript('{{ Storage::disk('s3')->url($post->voice_script) }}', this); event.stopPropagation();">音声再生</button>
+            <button onclick="playVoiceScript('{{ Storage::disk('s3')->url($post->voice_script) }}', this); event.stopPropagation();">現在の音声</button>
             @else
             <span>音声なし</span>
             @endif
@@ -78,20 +78,25 @@
 
   let currentAudio = null; // 現在再生中のオーディオオブジェクトを保持するための変数
 
-  function playVoiceScript(url, buttonElement) {
-    if (currentAudio) {
-      currentAudio.pause(); // すでに再生中の音声があれば、停止
-      currentAudio = null;
-      return;
-    }
-
-    const audio = new Audio(url);
-    audio.play();
-
-    audio.onended = function() {
-      currentAudio = null;
-    };
-
-    currentAudio = audio;
+function playVoiceScript(url, buttonElement) {
+  if (currentAudio) {
+    currentAudio.pause(); // すでに再生中の音声があれば、停止
+    buttonElement.textContent = "現在の音声"; // ボタンのラベルを「現在の音声」に変更
+    currentAudio = null;
+    return;
   }
+
+  const audio = new Audio(url);
+  audio.play();
+
+  buttonElement.textContent = "停止"; // ボタンのラベルを「停止」に変更
+
+  audio.onended = function() {
+    buttonElement.textContent = "現在の音声"; // 音声が終了したら、ボタンのラベルを「現在の音声」に変更
+    currentAudio = null;
+  };
+
+  currentAudio = audio;
+}
+
 </script>
